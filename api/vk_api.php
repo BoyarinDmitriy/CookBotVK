31b0246c9807b26a76fdab3755836433fdb3cf55
Dmitriy <boyarin.dmitriy1998@gmail.com>
2018-06-19 23:30:10 +0300

    ok

--- api/vk_api.php
+++ api/vk_api.php
@@ -1,13 +1,5 @@
 <?php
 
-define('VK_API_ENDPOINT', 'https://api.vk.com/method/');
-define('VK_API_VERSION', '5.50');
-define('VK_API_SERVICE_TOKEN', 'd9c02b26d9c02b26d9c02b26fdd9a4f3f9dd9c0d9c02b2682e7c02f21adc58266ea11c7');
-define('GROUP_DOMAIN', 'sekretwomen');
-define('POST_ADDRESS','https://vk.com/sekretwomen?w=wall-49724592_');
-define('VK_API_ACCESS_TOKEN', '297aa82a72a59f70e54c9efc578764475cca3c8702ff16ba0464a6b76c89716abd4fbeba551d4668f2a43');
-
-
 function vkApi_messagesSend($peer_id, $message) {
     return _vkApi_call('messages.send', array(
         'peer_id' => $peer_id,
