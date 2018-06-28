b04a1fcbc0fb7fd94df2af2bcc2e55d6fb118241
Dmitriy <boyarin.dmitriy1998@gmail.com>
2018-06-28 20:21:21 +0300

    ok

--- bot.php
+++ bot.php
@@ -4,6 +4,16 @@ require_once 'recipe_generator.php';
 require_once 'vk_api.php';
 
 function bot_sendMessage($user_id, $message) {
-    $recipe = get_recipe($message);
-    vkApi_messagesSend($user_id, $recipe);
+    switch ($message) {
+        case 'Начать':
+            $start_message = 'Привет!'.PHP_EOL.
+                'Так-с, давай найдем тебе рецепт'.PHP_EOL.
+                'Напиши в строчку через запятую ингредиенты (например: огурец, помидор, лук)';
+            vkApi_messagesSend($user_id, $start_message);
+            break;
+        default:
+            $recipe = get_recipe($message);
+            vkApi_messagesSend($user_id, $recipe);
+            break;
+    }
 }
