0b55b6f031dae357a744469fec0a3cdbee5c700c
Dmitriy <boyarin.dmitriy1998@gmail.com>
2018-06-29 19:51:58 +0300

    ok

--- recipe_generator.php
+++ recipe_generator.php
@@ -15,33 +15,38 @@ function get_recipe($query, $count_of_recipes) {
     while($counter > 0){
         $recipes = vkApi_wallSearch(GROUP_DOMAIN, $query);
         $recipes_count = $recipes['count'];
-        if($recipes_count > 1) {
-            if($recipes_count > 19) {
-                $recipes_count = 19;
-            }
-            if($is_complete_coincidence) {
-                $recipe = '';
-                if($recipes_count > $count_of_recipes){
-                    $recipes_count = $count_of_recipes;
+        if ($recipes_count != null) {
+            if($recipes_count > 1) {
+                if($recipes_count > 19) {
+                    $recipes_count = 19;
                 }
-                for($i = 0; $i < $recipes_count; $i++) {
-                    $recipe = $recipe.POST_ADDRESS.$recipes['items'][$i]['id'].PHP_EOL;
-                }
+                if($is_complete_coincidence) {
+                    $recipe = '';
+                    if($recipes_count > $count_of_recipes){
+                        $recipes_count = $count_of_recipes;
+                    }
+                    for($i = 0; $i < $recipes_count; $i++) {
+                        $recipe = $recipe.POST_ADDRESS.$recipes['items'][$i]['id'].PHP_EOL;
+                    }
 
+                } else {
+                    $recipe = $recipes['items'][rand(0, $recipes_count - 1)]['text'];
+                }
+                break;
             } else {
-                $recipe = $recipes['items'][rand(0, $recipes_count - 1)]['text'];
+                $is_complete_coincidence = false;
+                $query = '';
+                unset($ingredients[rand(0, count($ingredients) - 1)]);
+                foreach($ingredients as $ingredient){
+                    $query = $query.$ingredient.'  ';
+                }
+                $query = str_replace('  ', ', ', trim($query));
+                $ingredients = explode(', ', $query);
+                $counter--;
             }
-            break;
         } else {
-            $is_complete_coincidence = false;
-            $query = '';
-            unset($ingredients[rand(0, count($ingredients) - 1)]);
-            foreach($ingredients as $ingredient){
-                $query = $query.$ingredient.'  ';
-            }
-            $query = str_replace('  ', ', ', trim($query));
-            $ingredients = explode(', ', $query);
-            $counter--;
+            $recipe = 'Приходи за новым рецептом завтра!';
+            break;
         }
     }
     return $recipe;
